6234bfa77fc429c92fd8db029574fbed7d803f80
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-20 14:51:03 +0200

    Added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,8 @@ export default {
   <AppMain />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+  @import 'bootstrap/scss/bootstrap';
+  @import './styles/general.scss';
+
 </style>
