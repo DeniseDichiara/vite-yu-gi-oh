d6ea4287a66635375a8920c8bb4b79f5e7cd2911
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-20 19:44:45 +0200

    Added dropdown menù

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,20 @@
 <template>
-    <main>
-        <h1>Main</h1>
+    <main class="pt-4">
+
+
+        <!--*Drop down menù-->
+        <div class="dropdown ms-5">
+            <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Alien
+            </button>
+            <ul class="dropdown-menu">
+                <li><button class="dropdown-item" type="button">Action</button></li>
+                <li><button class="dropdown-item" type="button">Another action</button></li>
+                <li><button class="dropdown-item" type="button">Something else here</button></li>
+            </ul>
+        </div>
+
+
         <CharacterList />
     </main>
 </template>
@@ -13,7 +27,7 @@ import CharacterList from './CharacterList.vue';
 
 export default {
     name: 'AppMain',
-    data(){
+    data() {
         return {
 
         }
@@ -25,7 +39,4 @@ export default {
 </script>
 
 
-<style lang="scss">
-    
-    
-</style>
+<style lang="scss"></style>
