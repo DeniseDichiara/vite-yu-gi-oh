26bc648c81f250f4fc21779def8b9fafafde07d7
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-20 17:54:18 +0200

    axios call

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -25,7 +25,7 @@ export default {
     //**Chiamata API, mi serve AXIOS */
     created() {
 
-        axios.get('')
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
             .then(function (response) {
                 // handle success
                 console.log(response);
